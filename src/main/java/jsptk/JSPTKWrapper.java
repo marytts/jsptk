6c698226e333d11f626ab55edd96d6f869ced9c8
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2018-07-31 12:22:37 +0200

    Add matrix operations

--- src/main/java/jsptk/JSPTKWrapper.java
+++ src/main/java/jsptk/JSPTKWrapper.java
@@ -12,7 +12,9 @@ public class JSPTKWrapper
     {
     }
 
-
+    /**********************************************************************
+     *** Vector operations
+     **********************************************************************/
     public double[] c2acr(double[] c, int order, int fftlen) {
         SWIGTYPE_p_double c_sp = JSPTKWrapper.java2swig(c);
         SWIGTYPE_p_double r_sp = Sptk.new_double_array(order);
@@ -21,8 +23,8 @@ public class JSPTKWrapper
         Sptk.c2acr(c_sp, c.length, r_sp, order, fftlen);
 
         double[] r = JSPTKWrapper.swig2java(r_sp, order);
-        Sptk.delete_double_array(c_sp);
-        Sptk.delete_double_array(r_sp);
+        JSPTKWrapper.clean(c_sp);
+        JSPTKWrapper.clean(r_sp);
 
         return r;
 
@@ -36,8 +38,8 @@ public class JSPTKWrapper
         Sptk.freqt(c_sp, c.length, c2_sp, order, alpha);
 
         double[] c2 = JSPTKWrapper.swig2java(c_sp, order);
-        Sptk.delete_double_array(c_sp);
-        Sptk.delete_double_array(c2_sp);
+        JSPTKWrapper.clean(c_sp);
+        JSPTKWrapper.clean(c2_sp);
 
         return c2;
     }
@@ -52,8 +54,8 @@ public class JSPTKWrapper
         Sptk.mc2b(mc_sp, b_sp, mc.length, alpha);
         double[] b = JSPTKWrapper.swig2java(b_sp, mc.length);
 
-        Sptk.delete_double_array(mc_sp);
-        Sptk.delete_double_array(b_sp);
+        JSPTKWrapper.clean(mc_sp);
+        JSPTKWrapper.clean(b_sp);
 
         return b;
     }
@@ -66,13 +68,92 @@ public class JSPTKWrapper
         // Convert c to
         Sptk.b2mc(b_sp, mc_sp, b.length, alpha);
         double[] mc = JSPTKWrapper.swig2java(mc_sp, b.length);
-        Sptk.delete_double_array(mc_sp);
-        Sptk.delete_double_array(b_sp);
+        JSPTKWrapper.clean(mc_sp);
+        JSPTKWrapper.clean(b_sp);
 
         return mc;
     }
 
 
+
+    /**********************************************************************
+     *** Vector operations
+     **********************************************************************/
+    public double[][] c2acr(double[][] c, int order, int fftlen) {
+        SWIGTYPE_p_p_double c_sp = JSPTKWrapper.java2swig(c);
+        SWIGTYPE_p_double r_sp = Sptk.new_double_array(order);
+        double[][] r = new double[c.length][order];
+
+        // Convert c to
+        for (int t=0; t<c.length; t++) {
+            Sptk.c2acr(Sptk.double_p_array_getitem(c_sp, t), c.length, r_sp, order, fftlen);
+            r[t] = JSPTKWrapper.swig2java(r_sp, order);
+        }
+
+        JSPTKWrapper.clean(c_sp, c.length);
+        JSPTKWrapper.clean(r_sp);
+
+        return r;
+
+    }
+
+    public double[][] freqt(double[][] c, int order, double alpha) {
+        SWIGTYPE_p_p_double c_sp = JSPTKWrapper.java2swig(c);
+        SWIGTYPE_p_double c2_sp = Sptk.new_double_array(order);
+        double[][] c2 = new double[c.length][order];
+
+        // Convert c to
+        for (int t=0; t<c.length; t++) {
+            Sptk.freqt(Sptk.double_p_array_getitem(c_sp, t), c.length, c2_sp, order, alpha);
+            c2[t] = JSPTKWrapper.swig2java(c2_sp, order);
+        }
+        JSPTKWrapper.clean(c_sp, c.length);
+        JSPTKWrapper.clean(c2_sp);
+
+        return c2;
+    }
+
+
+
+    public double[][] mc2b(double[][] mc, double alpha) {
+        SWIGTYPE_p_p_double mc_sp = JSPTKWrapper.java2swig(mc);
+        SWIGTYPE_p_double b_sp = Sptk.new_double_array(mc[0].length);
+        double[][] b = new double[mc.length][mc[0].length];
+
+        // Convert c to
+        for (int t=0; t<mc.length; t++) {
+            Sptk.mc2b(Sptk.double_p_array_getitem(mc_sp, t), b_sp, mc.length, alpha);
+            b[t] = JSPTKWrapper.swig2java(b_sp, mc[t].length);
+        }
+
+        JSPTKWrapper.clean(mc_sp, mc.length);
+        JSPTKWrapper.clean(b_sp);
+
+        return b;
+    }
+
+
+    public double[][] b2mc(double[][] b, double alpha) {
+        SWIGTYPE_p_p_double b_sp = JSPTKWrapper.java2swig(b);
+        SWIGTYPE_p_double mc_sp = Sptk.new_double_array(b[0].length);
+        double[][] mc = new double[b.length][b[0].length];
+
+        // Convert c to
+        for (int t=0; t<b.length; t++) {
+            Sptk.mc2b(Sptk.double_p_array_getitem(b_sp, t), mc_sp, b.length, alpha);
+            mc[t] = JSPTKWrapper.swig2java(mc_sp, b[t].length);
+        }
+
+        JSPTKWrapper.clean(b_sp, b.length);
+        JSPTKWrapper.clean(mc_sp);
+
+        return mc;
+    }
+
+
+    /**********************************************************************
+     *** JNI Utilities
+     **********************************************************************/
     private static double[][] swig2java(SWIGTYPE_p_p_double ar, int nb_rows, int nb_cols) {
         double[][] res = new double[nb_rows][nb_cols];
 
@@ -116,5 +197,18 @@ public class JSPTKWrapper
         }
 
         return res;
+    }
+
+
+
+
+    private static void clean(SWIGTYPE_p_p_double ar, int length) {
+        for (int t=0; t<length; t++)
+            Sptk.delete_double_array(Sptk.double_p_array_getitem(ar, t));
+        Sptk.delete_double_p_array(ar);
+    }
+
+    private static void clean(SWIGTYPE_p_double ar) {
+        Sptk.delete_double_array(ar);
     }
 }
